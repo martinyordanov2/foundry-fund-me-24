426b4a16d327fdaefcbdf5406ee7f6588b1f9777
martinyordanov2 <martinyordanov2@gmail.com>
2024-11-19 18:28:54 +0200

    changes after refactoring

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -3,16 +3,18 @@ pragma solidity ^0.8.18;
 
 import {Script} from "@forge-std/src/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
 
-    function run() external {
+    function run() external returns (FundMe){
+
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
 
         vm.startBroadcast();
-
-        new FundMe();
-        
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
-
+        return fundMe;
     }
 }
